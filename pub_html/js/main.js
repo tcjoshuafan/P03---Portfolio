aeee9cf75c1f16b1b07c586292e88187546c3219
Unknown <tcjoshuafan@gmail.com>
2018-03-25 21:46:11 -0700

    removed IE6 nav

--- pub_html/js/main.js
+++ pub_html/js/main.js
@@ -28,3 +28,17 @@ $(document).ready(function() {
         }
     });
 });
+
+setInterval(function(){
+  $("#glitch-business").toggle();
+  setTimeout(function(){
+    $("#glitch-business").toggle();
+  }, 1000)
+},2000);
+
+setInterval(function(){
+  $("#glitch-ux").toggle();
+  setTimeout(function(){
+    $("#glitch-ux").toggle();
+  }, 1000)
+},2000);
