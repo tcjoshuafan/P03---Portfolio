3c8d08a92c3366894491662926275cbc99ab717e
Unknown <tcjoshuafan@gmail.com>
2018-03-29 12:49:13 -0700

    interaction overhaul
    added button and tile effects, begun second sticky nav on project pages, changed body color

--- pub_html/js/main.js
+++ pub_html/js/main.js
@@ -20,4 +20,27 @@ $(function() {
 });
 
 
-var mixer = mixitup('.projects')
+$(document).ready(function(){
+	$('.bg-video').bgVideo();
+});
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction()
+};
+
+// Get the header
+var header = document.getElementById("second-sticky");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
