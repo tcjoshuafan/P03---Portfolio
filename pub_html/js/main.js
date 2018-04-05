e007fcf5ab1f280e9896f217c08b7faf4883aa60
Unknown <tcjoshuafan@gmail.com>
2018-04-05 13:43:30 -0700

    close as can be

--- pub_html/js/main.js
+++ pub_html/js/main.js
@@ -32,20 +32,20 @@ $('.content-toggle .filter-button').on('click', function (e) {
    }
  });
 
- // When the user scrolls the page, execute myFunction
- window.onscroll = function() {myFunction()};
-
- // Get the header
- var header = document.getElementById("stickynav");
-
- // Get the offset position of the navbar
- var sticky = header.offsetTop;
-
- // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
- function myFunction() {
-   if (window.pageYOffset >= sticky) {
-     header.classList.add("sticky");
-   } else {
-     header.classList.remove("sticky");
-   }
- }
+ // // When the user scrolls the page, execute myFunction
+ // window.onscroll = function() {myFunction()};
+ //
+ // // Get the header
+ // var header = document.getElementById("stickynav");
+ //
+ // // Get the offset position of the navbar
+ // var sticky = header.offsetTop;
+ //
+ // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+ // function myFunction() {
+ //   if (window.pageYOffset >= sticky) {
+ //     header.classList.add("sticky");
+ //   } else {
+ //     header.classList.remove("sticky");
+ //   }
+ // }
