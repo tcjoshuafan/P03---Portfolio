c9bfe8642c3a9fba58b5556649095d09761101aa
Unknown <tcjoshuafan@gmail.com>
2018-04-08 13:59:20 -0700

    test new sticky nav

--- pub_html/js/main.js
+++ pub_html/js/main.js
@@ -19,10 +19,6 @@ $(function() {
   });
 });
 
-$(document).ready(function(){
-	$('.bg-video').bgVideo();
-});
-
 $('.content-toggle .filter-button').on('click', function (e) {
    if ($(e.target).data('filter') === '.long') {
      $('body').addClass('long');
@@ -31,45 +27,39 @@ $('.content-toggle .filter-button').on('click', function (e) {
    }
  });
 
- // // When the user scrolls the page, execute myFunction
- // window.onscroll = function() {myFunction()};
  //
- // // Get the header
- // var header = document.getElementById("stickynav");
+ // $(document).ready(function() {
+	// 		// grab the initial top offset of the navigation
+	// 	   	var stickyNavTop = $('.stickynav').offset().top;
  //
- // // Get the offset position of the navbar
- // var sticky = header.offsetTop;
+	// 	   	// our function that decides whether the navigation bar should have "fixed" css position or not.
+	// 	   	var stickyNav = function(){
+	// 		    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
  //
- // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
- // function myFunction() {
- //   if (window.pageYOffset >= sticky) {
- //     header.classList.add("sticky");
- //   } else {
- //     header.classList.remove("sticky");
- //   }
- // }
+	// 		    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+	// 		    // otherwise change it back to relative
+	// 		    if (scrollTop > stickyNavTop) {
+	// 		        $('.stickynav').addClass('sticky');
+	// 		    } else {
+	// 		        $('.stickynav').removeClass('sticky');
+	// 		    }
+	// 		};
+ //
+	// 		stickyNav();
+	// 		// and run it again every time you scroll
+	// 		$(window).scroll(function() {
+	// 			stickyNav();
+	// 		});
+	// 	});
 
-
- $(document).ready(function() {
-			// grab the initial top offset of the navigation
-		   	var stickyNavTop = $('.stickynav').offset().top;
-
-		   	// our function that decides whether the navigation bar should have "fixed" css position or not.
-		   	var stickyNav = function(){
-			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-
-			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-			    // otherwise change it back to relative
-			    if (scrollTop > stickyNavTop) {
-			        $('.stickynav').addClass('sticky');
-			    } else {
-			        $('.stickynav').removeClass('sticky');
-			    }
-			};
-
-			stickyNav();
-			// and run it again every time you scroll
-			$(window).scroll(function() {
-				stickyNav();
-			});
-		});
+$(document).ready(function() {
+ 			$('.stickynav').scrollspy({
+ 				min: $('.stickynav').offset().top,
+ 				onEnter: function(element, position) {
+ 					$(".stickynav").addClass('sticky');
+ 				},
+ 				onLeave: function(element, position) {
+ 					$(".stickynav").removeClass('sticky');
+ 				}
+ 			});
+    });
