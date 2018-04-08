6a07749ab1d9dc1c9f854ae0d73730b362c96204
Unknown <tcjoshuafan@gmail.com>
2018-04-08 14:25:58 -0700

    scrollspy try 2

--- pub_html/js/main.js
+++ pub_html/js/main.js
@@ -27,39 +27,29 @@ $('.content-toggle .filter-button').on('click', function (e) {
    }
  });
 
- //
- // $(document).ready(function() {
-	// 		// grab the initial top offset of the navigation
-	// 	   	var stickyNavTop = $('.stickynav').offset().top;
- //
-	// 	   	// our function that decides whether the navigation bar should have "fixed" css position or not.
-	// 	   	var stickyNav = function(){
-	// 		    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
- //
-	// 		    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-	// 		    // otherwise change it back to relative
-	// 		    if (scrollTop > stickyNavTop) {
-	// 		        $('.stickynav').addClass('sticky');
-	// 		    } else {
-	// 		        $('.stickynav').removeClass('sticky');
-	// 		    }
-	// 		};
- //
-	// 		stickyNav();
-	// 		// and run it again every time you scroll
-	// 		$(window).scroll(function() {
-	// 			stickyNav();
-	// 		});
-	// 	});
 
-$(document).ready(function() {
- 			$('.stickynav').scrollspy({
- 				min: $('.stickynav').offset().top,
- 				onEnter: function(element, position) {
- 					$(".stickynav").addClass('sticky');
- 				},
- 				onLeave: function(element, position) {
- 					$(".stickynav").removeClass('sticky');
- 				}
- 			});
-    });
+ $(document).ready(function() {
+			// grab the initial top offset of the navigation
+		   	var stickyNavTop = $('.stickynav').offset().top;
+
+		   	// our function that decides whether the navigation bar should have "fixed" css position or not.
+		   	var stickyNav = function(){
+			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+
+			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			    // otherwise change it back to relative
+			    if (scrollTop > stickyNavTop) {
+			        $('.stickynav').addClass('sticky');
+			    } else {
+			        $('.stickynav').removeClass('sticky');
+			    }
+			};
+
+			stickyNav();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyNav();
+			});
+		});
+
+$(".stickynav").scrollspy({activeClass: '.activeNav'});
